1ddd107731876da051675e52e8ef803b5c162fe3
Jeffery-Zhou <zhoupeng2014@gmail.com>
2020-10-08 21:46:33 +0800

    update

--- Convertor.py
+++ Convertor.py
@@ -9,7 +9,6 @@ import os
 
 SCALE_FACTOR = 32 
 # put this file under the path of the folder containing .svs file. 
-
 def fileProcessor(filepath, output_path):
     slide = openslide.open_slide(filepath)
     large_w, large_h = slide.dimensions
@@ -21,8 +20,9 @@ def fileProcessor(filepath, output_path):
     img = whole_slide_image.resize((new_w, new_h), PIL.Image.BILINEAR)
     imageio.imwrite(output_path, img)
 
-ROOT_PATH = "D:/Python34/news"
-OUTPUT_PATH = ""
+
+ROOT_PATH = "/Volumes/MedImage"
+OUTPUT_PATH = "./images"
 
 files= os.listdir(ROOT_PATH) 
 
@@ -33,10 +33,21 @@ for file in files:
          if "svs" in file:
             file_path = ROOT_PATH + "/" + file
             file_path_lst.append(file_path)
-            output_path = OUTPUT_PATH + "/" + file[:-3] + '.png'
+            output_path = OUTPUT_PATH + "/" + file[:-4] + '.png'
             output_path_lst.append(output_path)
             
-for i in range(len(file_path_lst)):
-    fileProcessor(file_path_lst[i], output_path_lst[i])
+# for i in range(10):
+#     print(file_path_lst[i])
+#     print(output_path_lst[i])           
+for i in range(100, 200):
+    try:
+        fileProcessor(file_path_lst[i], output_path_lst[i])
+    except IOError:
+        print(i, "has Error, and the path: ", file_path_lst[i])
+    else:
+        print(i, "Finished")
+    
+    # Issue index: 48  '/Volumes/MedImage/TCGA-GM-A3XL-01Z-00-DX1.CCE8AA1D-9194-4E49-9546-DBF25A35847C.svs'
+    # 
     
 
